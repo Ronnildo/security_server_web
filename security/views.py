9043ab33462c11bbfc139793d3328d909ebeb21d
Ronnildo <ronildosp03@.com>
2024-01-30 16:05:25 -0300

    Add: refactor login page

--- security/views.py
+++ security/views.py
@@ -20,20 +20,20 @@ def login(req):
     return render(req, 'index.html', context)
 
 def home(req):
-    form = UserForm(req.POST or None)
-    if str(req.user) != 'AnonymousUser':
-        if str(req.method) == 'POST':
-            if form.is_valid():
-                messages.success('Login efetuado!!')
-                form = UserForm()
-            else:
-                messages.error('E-mail ou senha Incorretos!!')
-        context = {
-            'form': form
-        }
-        return render(req, 'home.html', context)
-    else:
-        return redirect('login')
+    return render(req, 'home.html')
+    # form = UserForm(req.POST or None)
+    # # if str(req.user) != 'AnonymousUser':
+    # if str(req.method) == 'POST':
+    #     if form.is_valid():
+    #         messages.success('Login efetuado!!')
+    #         form = UserForm()
+    #     else:
+    #         messages.error('E-mail ou senha Incorretos!!')
+    # context = {
+    #     'form': form
+    # }
+    # else:
+    #     return redirect('login')
 
 def details(req):
     return render(req, "details.html")
